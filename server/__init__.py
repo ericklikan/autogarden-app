459edb99d76f688b45c6afb3aa32cba1e627a33f
Erick <eri.lewo@hotmail.com>
2018-11-04 20:30:29 -0500

    Added CORS

--- server/__init__.py
+++ server/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
 socketio = SocketIO()
 
 
 def create_app(debug=False):
     app = Flask(__name__)
+    CORS(app)
     app.config['SECRET_KEY'] = 'secret!'
     app.debug = debug
 
